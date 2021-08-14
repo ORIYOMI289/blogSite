97e21df4db5980f0e0f6eb760fdb54206e87071c
ORIYOMI289 <mustaphaamidatoyindamola@gmail.com>
2021-08-14 06:12:38 +0100

    blog site

--- routes/articles.js
+++ routes/articles.js
@@ -2,57 +2,86 @@ const express = require('express');
 const router = express.Router() ;
 const articleModel = require('../model/newarticles') ;
 
-const articlesContent = [{
-    title: "new post",
-    subtitle: "subtitle",
-    content: "text description",
-    date: new Date() 
-}]
+// const articlesContent = [{
+//     title: "new post",
+//     subtitle: "subtitle",
+//     content: "text description",
+//     date: new Date() 
+// }]
 
 router.get('/newarticles', (req, res) => {
     res.render('newarticles')
 })
 
-router.get('/articles', async (req, res) => {
-    const article = await articleModel.find().sort({ 
-        createdAt: 'desc'
-    })
-    res.render("articlesDisplay", {Articles: article}) 
+router.get('/articleEdit/:id', async (req, res) => {
+    const article = await articleModel.findById(req.params.id)
+    res.render('Edit', {article : article })
 }) ;
 
-router.get('/articles/:slug', async (req, res) => {
+
+router.get('/articles', (req, res, next) => {
+           next()
+        }, ArticleCOntent("articlesDisplay"))
+
+router.get('/userArticle', (req, res, next) => {
+            next()
+        }, ArticleCOntent('users'))
+
+        // const routes = [ '/articles/:slug', '/articles/:id'] ;
+router.get( '/articles/:slug', async (req, res) => {
     const Article = await articleModel.findOne({ slug: req.params.slug }) ;
     if (Article == null) return res.redirect('/articles') ;
+    
     res.render('articleShow', {Articles: Article})
 })
 
-router.post( '/articles', async (req, res) => {
-    let article = new articleModel({
-        title: req.body.title,
-        subtitle: req.body.subtitle,
-        description: req.body.description,
-        markdown: req.body.markdown
-    }) ;
+router.post( '/articles', (req, res, next) => {
 
-    try { 
-        article = await article.save() ;
-        res.redirect(`/articles/${article.slug}`)
-    } catch (err) {
-        res.render('newarticles', {article: article}) ; 
-    }
-    
-})
+    next() ;
+}, postOrEditArticle('articleDisplay') )
+
+router.put( '/articlesEdit/:id', (req, res, next) => {
+
+    next() ;
+}, postOrEditArticle('Edit') )
 
 router.delete('/:id', async (req, res) => {
     await articleModel.findByIdAndDelete(req.params.id) 
     res.redirect('/articles')
 })
 
-router.get('/userArticle',async (req, res) => {
-    const article = await articleModel.find().sort({ 
-        createdAt: 'desc'
-    })
-    res.render("users", {Articles: article}) ;
-}) ;
+
+
+function postOrEditArticle(path) {
+    return async (req, res) => {
+        let article = new articleModel({
+            title: req.body.title,
+            subtitle: req.body.subtitle,
+            description: req.body.description,
+            markdown: req.body.markdown
+        }) ;
+    
+        try { 
+            article = await article.save() ;
+            res.redirect(`/articles/${article.slug}`)
+        } catch (err) {
+            res.render(path, {article: article}) ; 
+        }
+    }
+} ;
+function ArticleCOntent(paths) {
+    return async (req, res) => {
+        const article = await articleModel.find().sort({ 
+            createdAt: 'desc'
+        }) ;
+        try {
+            res.render(paths, {Articles: article}) ;
+           
+        } catch(err) {
+            console.log('go')
+        }
+       
+    }
+} ;
 
 module.exports = router ;
